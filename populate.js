e0c60a72de1a87215844f3fb2b7142df7c736e42
oliver-southon <oliversouthon@gmail.com>
2023-01-28 20:50:45 +1100

    third-commit-toggle

--- populate.js
+++ populate.js
@@ -1,38 +1,44 @@
-var showAllFish = false;
+// Javascript for table population
+var indexCount = 1;
+var date = new Date();
+var toggle = true;
+populateTable();
 
-document.getElementById("toggle-button").addEventListener("click", function() {
-    showAllFish = !showAllFish; // toggle the value of showAllFish
-    if(showAllFish){
-        // Clear the current table
-        document.getElementById("january-table").innerHTML = "";
-        // Re-populate the table with all the rows
+const toggleBtn = document.getElementById("toggle-btn");
+let buttonClicked = false;
+
+toggleBtn.addEventListener("click", function(){
+    if (buttonClicked === false) {
+        toggle = !toggle;
+        toggleBtn.innerHTML = "Toggle: <em>Showing All Fish</em>";
+        buttonClicked = true;
+        populateTable();
+      } else {
+        toggle = !toggle;
+        toggleBtn.innerHTML = "Toggle: <em>Showing Available Fish Only</em>";
+        buttonClicked = false;
+        populateTable();
+      }
+});
+
+function populateTable() {
+    document.getElementById('january-table').querySelector('tbody').innerHTML = '';
+    indexCount = 1;
+    fetch('./newdata.csv')
+    .then(response => response.text())
+    .then(data => {
+        // Parse the CSV data
+        const rows = data.split('\n');
+        const headers = rows[0].split(',');
+
+        // Add the data to the table
         for (let i = 1; i < rows.length; i++) {
             const cells = rows[i].split(',');
             const row = document.createElement('tr');
-            // add index to row
-            const th = document.createElement("th");
-            th.setAttribute("scope", "row");
-            th.innerText = i;
-            row.appendChild(th);
-            // add all the other stuff to the row
-            for (let j = 0; j < cells.length; j++) {
-                const cell = document.createElement('td');
-                cell.innerHTML = cells[j];
-                row.appendChild(cell);
-            }
-            document.getElementById('january-table').querySelector('tbody').appendChild(row);
-        }
-    }else{
-        // Clear the current table
-        document.getElementById("january-table").innerHTML = "";
-        // Re-populate the table with only the fish that are in season
-        var date = new Date();
-        var indexCount = 1;
-        for (let i = 1; i < rows.length; i++) {
-            const cells = rows[i].split(',');
-            const row = document.createElement('tr');
+
             var fishTime = cells[cells.length-1].trim();
             var curHour = date.getHours();
+
             // transform fishTime into a usable array
             switch (fishTime) {
                 case "All day":
@@ -56,13 +62,13 @@ document.getElementById("toggle-button").addEventListener("click", function() {
                 default:
                     var fishHoursArray = [];
             }
-            console.log(fishHoursArray);
 
             // add index to row
             const th = document.createElement("th");
             th.setAttribute("scope", "row");
-            th.innerText = i;
-            row.appendChild(th);
+            th.innerText = indexCount;
+            row.appendChild(th)
+
             // add all the other stuff to the row
             for (let j = 0; j < cells.length; j++) {
                 const cell = document.createElement('td');
@@ -70,10 +76,17 @@ document.getElementById("toggle-button").addEventListener("click", function() {
                 row.appendChild(cell);
             }
 
-            if (fishHoursArray.includes(curHour) == true) {
+            // if toggle is true and its in the correct time, add it to the table
+            if (toggle == true) {
+                if (fishHoursArray.includes(curHour) == true) {
+                    document.getElementById('january-table').querySelector('tbody').appendChild(row);
+                    indexCount += 1;
+                }
+            } else {
                 document.getElementById('january-table').querySelector('tbody').appendChild(row);
                 indexCount += 1;
             }
         }
-    }
-});
+    });
+}
+    
